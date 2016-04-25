21f9dae28ee882f6b2180e7b380fbd8307e924b4
bertthedefender <simon@armytruck.co.uk>
2016-04-25 21:13:42 +0100

    Fields of first record correctly identified

--- app/src/test/java/sky/skyweatherapp/CityRetrievalTests.java
+++ app/src/test/java/sky/skyweatherapp/CityRetrievalTests.java
@@ -16,10 +16,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class CityRetrievalTests {
 
-
-
-
-
     @Test
     public void givenAJsonResponseTheModelIdentifiesTheCorrectNumberOfCities() throws JSONException {
 
@@ -28,6 +24,22 @@ public class CityRetrievalTests {
         List<CityData> cityResults = model.parseCitySearchResponse(TestData.sampleCityData);
 
         assertThat(cityResults.size(), is(4));
+
+    }
+
+    @Test
+    public void givenAJsonResponseTheModelIsPopulatedWithTheCorrectData() throws JSONException {
+        DataModel model = new DataModel();
+        List<CityData> cityResults = model.parseCitySearchResponse(TestData.sampleCityData);
+
+        CityData first = cityResults.get(0);
+        CityData second = cityResults.get(1);
+        CityData third = cityResults.get(2);
+        CityData fourth = cityResults.get(3);
+
+        assertThat(first.getId(), is(5089178L));
+        assertThat(first.getName(), is("Manchester"));
+        assertThat(first.getCountry(), is("US"));
 
     }
 
@@ -42,7 +54,12 @@ public class CityRetrievalTests {
             JSONArray cities = json.getJSONArray("list");
 
             for (int i=0;i<cities.length();i++) {
-                returnValue.add(new CityData());
+
+                long id  = cities.getJSONObject(i).getLong("id");
+                String name = cities.getJSONObject(i).getString("name");
+                String country = cities.getJSONObject(i).getJSONObject("sys").getString("country");
+
+                returnValue.add(new CityData(id,name,country));
             }
 
             return returnValue;
@@ -51,5 +68,26 @@ public class CityRetrievalTests {
 
     private class CityData {
 
+        private long id;
+        private String name;
+        private String country;
+
+        public CityData(long id, String name, String country) {
+            this.id = id;
+            this.name = name;
+            this.country = country;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCountry() {
+            return country;
+        }
     }
 }

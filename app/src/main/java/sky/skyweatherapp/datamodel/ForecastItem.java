1a9ea6998f07e49545beba6b1556b609f03977af
mcdons20 <simon.mcdonough@bbc.co.uk>
2016-04-27 10:03:01 +0100

    minor refactors

--- app/src/main/java/sky/skyweatherapp/datamodel/ForecastItem.java
+++ app/src/main/java/sky/skyweatherapp/datamodel/ForecastItem.java
@@ -12,6 +12,45 @@ public class ForecastItem {
         return direction;
     }
 
+
+    public String getCardinalDirectionString(double direction) {
+
+        if (direction >= 347.75 || direction < 11.25) {
+            return "N";
+        } else if (direction >= 11.25 && direction < 33.75) {
+            return "NNE";
+        } else if (direction >= 33.75 && direction < 56.25) {
+            return "NE";
+        } else if (direction >= 56.25 && direction < 78.75) {
+            return "ENE";
+        } else if (direction >= 78.75 && direction < 101.25) {
+            return "N";
+        } else if (direction >= 101.25 && direction < 123.75) {
+            return "ESE";
+        } else if (direction >= 123.75 && direction < 146.25) {
+            return "SE";
+        } else if (direction >= 146.25 && direction < 168.75) {
+            return "SSE";
+        } else if (direction >= 168.75 && direction < 191.25) {
+            return "S";
+        } else if (direction >= 191.25 && direction < 213.75) {
+            return "SSW";
+        } else if (direction >= 213.75 && direction < 236.25) {
+            return "SW";
+        } else if (direction >= 236.25 && direction < 258.75) {
+            return "WSW";
+        } else if (direction >= 258.75 && direction < 281.25) {
+            return "W";
+        } else if (direction >= 281.25 && direction < 303.75) {
+            return "WNW";
+        } else if (direction >= 303.75 && direction < 326.25) {
+            return "NW";
+        } else if (direction >= 326.25 && direction < 347.75) {
+            return "NNW";
+        }
+        return "UNK";
+    }
+
     public double getSpeed() {
         return speed;
     }
--- app/src/main/java/sky/skyweatherapp/view/ForecastActivity.java
+++ app/src/main/java/sky/skyweatherapp/view/ForecastActivity.java
@@ -44,6 +44,8 @@ public class ForecastActivity extends AppCompatActivity implements ForecastView,
             networkFetcherService = binder.getService();
 
             networkFetcherService.setCallback(ForecastActivity.this);
+
+            bound=true;
         }
 
         @Override
@@ -51,12 +53,16 @@ public class ForecastActivity extends AppCompatActivity implements ForecastView,
 
         }
     };
+    private boolean bound = false;
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onDestroy() {
+        super.onDestroy();
 
-        unbindService(serviceConnection);
+        if (bound) {
+            unbindService(serviceConnection);
+            bound = false;
+        }
     }
 
     private DataRetrievedCallback dataRetrievedCallback;
@@ -174,7 +180,7 @@ public class ForecastActivity extends AppCompatActivity implements ForecastView,
 
             date.setText(dateFormat.format(forecastItem.getDatetime() * 1000));
             time.setText(timeFormat.format(forecastItem.getDatetime() * 1000));
-            direction.setText(getCardinalString(forecastItem.getDirection()));
+            direction.setText(forecastItem.getCardinalDirectionString(forecastItem.getDirection()));
             speed.setText(String.format("%.2f m/s", forecastItem.getSpeed()));
 
             //Hide date except on first forecast of day
@@ -186,43 +192,5 @@ public class ForecastActivity extends AppCompatActivity implements ForecastView,
             }
 
         }
-    }
-
-    private String getCardinalString(double direction) {
-
-        if (direction>=347.75 || direction<11.25) {
-            return "N";
-        } else if (direction>=11.25 && direction<33.75) {
-            return "NNE";
-        } else if (direction>=33.75 && direction<56.25) {
-            return "NE";
-        }else if (direction>=56.25 && direction<78.75) {
-            return "ENE";
-        }else if (direction>=78.75 && direction<101.25) {
-            return "N";
-        }else if (direction>=101.25 && direction<123.75) {
-            return "ESE";
-        }else if (direction>=123.75 && direction<146.25) {
-            return "SE";
-        }else if (direction>=146.25 && direction<168.75) {
-            return "SSE";
-        }else if (direction>=168.75 && direction<191.25) {
-            return "S";
-        }else if (direction>=191.25 && direction<213.75) {
-            return "SSW";
-        }else if (direction>=213.75 && direction<236.25) {
-            return "SW";
-        }else if (direction>=236.25 && direction<258.75) {
-            return "WSW";
-        }else if (direction>=258.75 && direction<281.25) {
-            return "W";
-        }else if (direction>=281.25 && direction<303.75) {
-            return "WNW";
-        }else if (direction>=303.75 && direction<326.25) {
-            return "NW";
-        }else if (direction>=326.25 && direction<347.75) {
-            return "NNW";
-        }
-        return "UNK";
     }
 }
